e54995bd88229a63ff23727efbf103938fada21b
Meryem Ünaldı <unaldimeryem747@gmail.com>
2026-07-01 13:52:43 +0300

    fix: kaydedilmeyen kodlar kaydedildi ve güncellendi

--- ratelimiter.go
+++ ratelimiter.go
@@ -9,68 +9,64 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// rdb değişkeni bizim Redis bağlantımızı tutacak.
-// Başına * koyduk çünkü bu bir "Pointer" (İşaretçi). Bellekteki tek bir Redis bağlantısını temsil ediyor.
-var rdb *redis.Client
+var rdb *redis.Client //rdb:redis database
 var ctx = context.Background()
 
-// rateLimiterHandler gelen her isteği karşılayan ve sınırlandıran akıllı fonksiyondur
 func rateLimiterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	// 1. İsteği atan kullanıcının IP adresini yakalıyoruz
+	//İsteği atan kullanıcının IP adresini yakalıyoruz
 	userIP := r.RemoteAddr
 
-	// 2. Redis'teki sayacı 1 artırıyoruz (IP -> Key oluyor)
-	count, err := rdb.Incr(ctx, userIP).Result()
+	//Redisteki sayacı 1 artırıyoruz (IP -> Key oluyor)
+	count, err := rdb.Incr(ctx, userIP).Result() //redisin komutu Incr(increment)
 	if err != nil {
 		http.Error(w, "Veritabanı hatası", http.StatusInternalServerError)
-		return
+		return //key e bak eğer bu ip daha önce geldiyse +1 yap. gelmediyse otomatik 1 yap
 	}
 
 	if count == 1 {
-		rdb.Expire(ctx, userIP, 1*time.Minute)
-	}
+		rdb.Expire(ctx, userIP, 1*time.Minute) //expire,hafızadaki ömrünü ayarlıyor
+	} //1 dk dolduğunda otomatik sıfırlar
 
-	// Arkada takip edebilmek için VS Code terminaline de kimin kaçıncı isteği attığını yazalım:
 	fmt.Printf("[LOG] İstek Atan IP: %s | Toplam İstek Sayısı: %d\n", userIP, count)
 
-	// 3. Sınır Kontrolü (Hakkı bittiyse)
+	// kontrol kısmı
 	if count > 5 {
 		w.WriteHeader(http.StatusTooManyRequests)
 		fmt.Fprintf(w, "429 - Çok Fazla İstek Attınız!\n\n")
 		fmt.Fprintf(w, "Sistem Kaydı:\n")
 		fmt.Fprintf(w, "-> Sizin IP Adresiniz (Key): %s\n", userIP)
-		fmt.Fprintf(w, "-> Bu dakikadaki toplam istek sayınız: %d (Sınır: 5)\n", count)
+		fmt.Fprintf(w, "-> Bir dakikadaki toplam istek sayınız: %d (Sınır: 5)\n", count)
 		return
 	}
 
-	// 4. Sınırı aşmadıysa (Başarılıysa)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "200 - Başarılı! Hoş geldiniz.\n\n")
 	fmt.Fprintf(w, "Sistem Kaydı:\n")
 	fmt.Fprintf(w, "-> Sizin IP Adresiniz (Key): %s\n", userIP)
-	fmt.Fprintf(w, "-> Bu dakikadaki toplam istek sayınız: %d/5\n", count)
+	fmt.Fprintf(w, "-> Bir dakikadaki toplam istek sayınız: %d/5\n", count)
 }
 
 func main() {
-	// 1. Docker'da dün ayağa kaldırdığımız Redis sunucusuna pointer bağlantısı kuruyoruz
+
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Dün açtığımız port köprüsü
-		Password: "",               // Şifre belirlemediğimiz için boş bırakıyoruz
-		DB:       0,                // Varsayılan veritabanı odası
+		Addr:     "localhost:6379", //redise bağlandığımız port
+		Password: "",
+		DB:       0,
 	})
 
-	// 2. Bağlantının gerçekten çalışıp çalışmadığını test ediyoruz (Ping-Pong testi)
-	_, err := rdb.Ping(ctx).Result()
+	//6379 kapısından docker daki redise bir pinpon fırlatıyor gibi. ping gönderiyor
+	//eğer docker açık ve redis çalışıyorsa pong cevabı döner.
+	_, err := rdb.Ping(ctx).Result() //baştaki "_" pong cevbını çöpe atıyor. kaydetmiyor gereksşz diye
 	if err != nil {
 		fmt.Println("Docker Redis sunucusuna bağlanılamadı! Lütfen Docker'ın açık olduğundan emin olun.", err)
 		return
 	}
-	fmt.Println("Docker Redis bağlantısı başarıyla kuruldu! 🚀")
+	fmt.Println("Docker Redis bağlantısı başarıyla kuruldu!")
 
 	// "/" endpoint'ine gelen istekleri bizim akıllı rateLimiterHandler fonksiyonuna yönlendiriyoruz
 	http.HandleFunc("/", rateLimiterHandler)
